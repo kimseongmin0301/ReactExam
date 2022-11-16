8640a875330f4400711760a340bfbc738652c711
kimseongmin0301 <hippo159753@gmail.com>
2022-11-16 22:26:10 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Button from "./Button"
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  
+  useEffect(() => {
+    console.log("only once");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("keyword change");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter change");
+  }, [counter]);
+  useEffect(() => {
+    console.log("doble change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} type="text" onChange={onChange} placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
